a9285cd142bc253405a17c64b3189ad55643a083
sentxd <sentxd@gmail.com>
2026-01-02 19:42:07 +1100

    added inetutils

--- configuration.nix
+++ configuration.nix
@@ -164,6 +164,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     neovim
+    inetutils
     git
     qemu_kvm
     virt-manager
