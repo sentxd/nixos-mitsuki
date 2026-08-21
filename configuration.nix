537e9cf69f511469c965e576be2834271d981641
sentxd <sentxd@gmail.com>
2026-08-21 18:21:26 +1000

    added ssh key hermes

--- configuration.nix
+++ configuration.nix
@@ -431,7 +431,7 @@ in
     description = "Sentinel";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbWX+YoGhWvG0K/nSSeWPqDFSMJnb3FzetVlBsQ8hnp hermes@sentinel-vm"
-    ]
+    ];
     extraGroups = [ "libvirtd" "networkmanager" "wheel" "kvm" "docker" ];
     packages = with pkgs; [
       kdePackages.kate
